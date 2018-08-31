9362a9aad4669d1fce11688bd833246d171aac0f
Tamer <32287010+tmmorad@users.noreply.github.com>
2018-08-30 22:23:02 -0700

    the getRandomQuote function is working and printing to console

--- js/script.js
+++ js/script.js
@@ -7,39 +7,50 @@ Tamer Morad
 // Create the array of quote objects and name it quotes
 var quotes = [
   {
-    quote: 'quote1',
-    source: 'author',
-    citation: '',
-    year: 2018,
+    quote: 'Thousands of tired, nerve-shaken, over-civilized people are beginning to find out that going to the mountains is going home; that wildness is a necessity',
+    source: 'John Muir',
+    citation: 'Our National Parks',
+    year: 1923,
+    tags:['book', 'nature', 'wisdom']
 
   },
   {
-    quote: 'quote2',
-    source: 'author',
-    citation: '',
-    year: 2018,
+    quote: 'Fear is a force that sharpens your senses. Being afraid is a state of paralysis in which you can\'t do anything.',
+    source: 'Marcus Luttrell',
+    citation: 'Service: A Navy SEAL at War',
+    year: 2011,
+    tags:['warrior', 'book', 'wisdom']
 
   },
   {
-    quote: 'quote3',
-    source: 'author',
-    citation: '',
-    year: 2018,
+    quote: 'There\'s a difference between knowing the path and walking the path.',
+    source: 'Morpheus',
+    citation: 'The Matrix',
+    year: 1999,
+    tags:['movie', 'wisdom']
 
   },
   {
-    quote: 'quote4',
-    source: 'author',
-    citation: '',
-    year: 2018,
+    quote: 'When you have a wild wolf living under your skin, you are less likely to beintimidated by reality.',
+    source: 'Daniele Bolelli',
+    citation: 'On the Warrior\'s Path',
+    year: 2008,
+    tags:['warrior', 'wild', 'wisdom', 'book']
 
   },
   {
-    quote: 'quote5',
-    source: 'author',
+    quote: 'You cant\'t go back and change the beginning, but you can start where you are and change the ending.',
+    source: 'C.S. Lewis',
     citation: '',
-    year: 2018,
-    //tags:['warrior', 'funny', 'wisdom']
+    year:'',
+    tags:['inspirational', 'wisdom']
+  },
+  {
+    quote: 'Maybe you\'ve been assigned this mountain to show others it can be moved. ',
+    source: 'Unknown',
+    citation: '',
+    year:'',
+    tags:['inspirational', 'wisdom', 'nature']
   }
 ];
 
@@ -55,16 +66,22 @@ function print(message, id){
 function getRandomQuote(quotes){
   var maxnum = quotes.length;
   var quoteNum = Math.floor(Math.random() * maxnum);
-  console.log(quoteNum);
+  console.log(quoteNum+1);
   for(var info in quotes[quoteNum]){
     pickedQuote.push(quotes[quoteNum][info]);
   }
-  console.log(pickedQuote);
+  console.log(pickedQuote);//remove this before submit
+  return pickedQuote;
 }
-getRandomQuote(quotes);
+
 // Create the printQuote funtion and name it printQuote
+/*
+function printQuote(){
+  getRandomQuote(quotes);
 
-
+}
+*/
+getRandomQuote(quotes);
 
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
